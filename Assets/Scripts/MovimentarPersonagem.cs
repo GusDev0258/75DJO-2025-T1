81f6df1d3b7979ade4ea7ffee3d505add3576680
gusdev0258 <16326176760@edu.udesc.br>
2025-03-08 17:51:51 -0300

    initial commit Starting the tutorial

---
+++ Assets/Scripts/MovimentarPersonagem.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentarPersonagem : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    public float jumpHeight = 6f;
+
+    public float gravity = -20f;
+
+    public Transform groundCheck;
+
+    public float sphereRadius = 0.4f;
+    public LayerMask groundMask;
+
+    public bool isOnTheGround;
+
+    private Vector3 fallingSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isOnTheGround = Physics.CheckSphere(groundCheck.position, sphereRadius, groundMask);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void OnGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(groundCheck.position, sphereRadius);
+    }
+}
